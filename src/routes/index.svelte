ef07720841937f65ff35569c1f313364758a76cd
wpcasarin <72427968+wpcasarin@users.noreply.github.com>
2022-04-19 17:17:59 -0300

    add tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	// your script goes here
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<style>
+	/* your styles go here */
+</style>
