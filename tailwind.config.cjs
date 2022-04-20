dbca06187a351a348819989de24788094b955570
wpcasarin <72427968+wpcasarin@users.noreply.github.com>
2022-04-20 08:54:28 -0300

    add tailwind theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,21 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		screens: {
+			xs: '375px',
+			xl: '1440px'
+		},
+		colors: {
+			primary: 'hsl(180, 66%, 49%)',
+			secondary: 'hsl(257, 27%, 26%)',
+			warning: 'hsl(0, 87%, 67%)',
+			neutral: {
+				100: 'hsl(0, 0%, 75%)',
+				200: 'hsl(257, 7%, 63%)',
+				700: 'hsl(255, 11%, 22%)',
+				900: 'hsl(260, 8%, 14%)'
+			}
+		},
 		extend: {}
 	},
 	plugins: []
