5a55cbc595a731cd8cfb9022a5fcd2d8603dd14b
wpcasarin <72427968+wpcasarin@users.noreply.github.com>
2022-04-21 18:17:50 -0300

    add cards

---
+++ src/components/layout/card/Card.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	export let title: string = 'Card title';
+	export let text: string =
+		'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae unde ut sequi ipsam, fuga est praesentium porro! Minus illum est, hic, aliquam nobis eos repellendus, velit eligendi suscipit odio blanditiis!';
+	export let line: boolean = false;
+	export let lineH: boolean = false;
+</script>
+
+<article class="relative rounded-md bg-white px-6 pb-10 xl:max-w-[350px]">
+	{#if line}
+		<span
+			class="absolute left-1/2 right-1/2 -top-1/2 z-[-1] h-32 w-2 -translate-x-1/2 bg-primary xl:hidden"
+		/>
+	{/if}
+	{#if lineH}
+		<span
+			class="absolute top-1/2 z-[-1] hidden h-2 w-[1000px] bg-primary xl:block"
+		/>
+	{/if}
+	<div
+		class="absolute left-1/2 right-1/2 -top-12 flex aspect-square w-[90px] -translate-x-1/2 items-center justify-center rounded-full bg-secondary bg-cover bg-center xl:left-20"
+	>
+		<slot><!-- icon --></slot>
+	</div>
+	<header
+		class="pt-20 text-center text-2xl font-bold text-neutral-700 xl:text-left"
+	>
+		<h2>{title}</h2>
+	</header>
+	<p
+		class="mt-5 text-center text-base font-medium text-neutral-200 xl:text-left"
+	>
+		{text}
+	</p>
+</article>
+
+<style>
+	/* your styles go here */
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,13 @@
 	import { ShortenLink } from '../components/shorten';
 	import { HeroSection } from '../components/layout/hero';
 	import { NavBar } from '../components/layout/navbar';
+	import { Card } from '../components/layout/card';
+	import icon1 from '$lib/assets/icon-brand-recognition.svg';
+	import icon2 from '$lib/assets/icon-detailed-records.svg';
+	import icon3 from '$lib/assets/icon-fully-customizable.svg';
 </script>
 
-<div class="container mt-10 sm:mt-14 xl:max-w-[1110px]">
+<div class="container mt-10 sm:mt-14 xl:max-w-[69.375rem]">
 	<NavBar />
 	<main>
 		<div class="mt-8 md:mt-20">
@@ -12,7 +16,7 @@
 		</div>
 		<div class="relative mt-20">
 			<div
-				class="absolute top-1/2 -left-1/2 -right-1/2 -z-10 h-screen bg-[#eff1f7]"
+				class="absolute top-1/2 -left-1/2 -right-1/2 -z-10 h-[200vh] bg-[#eff1f7]"
 			/>
 			<ShortenLink />
 		</div>
@@ -28,6 +32,37 @@
 				statistics dashboard.
 			</p>
 		</article>
+		<section
+			class="min-w-screen-xs md:min-w-screen-sm mx-auto mt-24 flex w-full flex-col justify-center gap-y-24 xl:mx-0 xl:flex-row xl:justify-between"
+		>
+			<div>
+				<Card
+					lineH={true}
+					title={'Brand Recognition'}
+					text={'Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.'}
+				>
+					<img src={icon1} alt="Card icon" />
+				</Card>
+			</div>
+			<div class="xl:mt-12">
+				<Card
+					line={true}
+					title={'Detailed Records'}
+					text={'Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.'}
+				>
+					<img src={icon2} alt="Card icon" />
+				</Card>
+			</div>
+			<div class="xl:mt-24">
+				<Card
+					line={true}
+					title={'Fully Customizable'}
+					text={' Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.'}
+				>
+					<img src={icon3} alt="Card icon" />
+				</Card>
+			</div>
+		</section>
 	</main>
 </div>
 
