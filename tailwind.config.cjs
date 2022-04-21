c7dce27c0d5e7c07b74e17ab9e5644729b349e0c
wpcasarin <72427968+wpcasarin@users.noreply.github.com>
2022-04-21 09:55:55 -0300

    fix theme config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,30 +1,29 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		fontFamily: {
+			sans: ['Poppins', ' sans-serif']
+		},
 		container: {
 			center: true
 		},
-		screens: {
-			xs: '375px',
-			sm: '640px',
-			md: '768px',
-			lg: '1110px',
-			xl: '1280px'
-		},
-		colors: {
-			primary: 'hsl(180, 66%, 49%)',
-			secondary: 'hsl(257, 27%, 26%)',
-			warning: 'hsl(0, 87%, 67%)',
-			neutral: {
-				100: 'hsl(0, 0%, 75%)',
-				200: 'hsl(257, 7%, 63%)',
-				700: 'hsl(255, 11%, 22%)',
-				900: 'hsl(260, 8%, 14%)'
+		extend: {
+			screens: {
+				xs: '326px'
+			},
+			colors: {
+				primary: 'hsl(180, 66%, 49%)',
+				'primary-focus': 'hsl(180, 66%, 69%)',
+				secondary: 'hsl(257, 27%, 26%)',
+				warning: 'hsl(0, 87%, 67%)',
+				neutral: {
+					100: 'hsl(0, 0%, 75%)',
+					200: 'hsl(257, 7%, 63%)',
+					700: 'hsl(255, 11%, 22%)',
+					900: 'hsl(260, 8%, 14%)'
+				}
 			}
-		},
-		extend: {}
+		}
 	},
 	plugins: []
 };
